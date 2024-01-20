3da23d9069ddaf064ee51759ce28225d20d68d6e
Jesse <j.hoed@xs4all.nl>
2024-01-20 03:59:24 +0100

    Added some menu, linear gradient, some header and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core'
-import { RouterOutlet } from '@angular/router'
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
+import { FooterComponent } from './footer/footer.component'
+import { BlogComponent } from './blog/blog.component'
+import { HeaderComponent } from './header/header.component'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss',
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+    FooterComponent,
+    BlogComponent,
+    HeaderComponent,
+    CommonModule,
+  ],
 })
 export class AppComponent {
   title = 'GewichtEnVoedingInBalans'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { HomePageComponent } from './home-page/home-page.component'
+import { BlogComponent } from './blog/blog.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Home', component: HomePageComponent },
+  { path: 'blog', title: 'blog', component: BlogComponent },
+]
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule],
+// })
+// export class AppRoutingModule {}
