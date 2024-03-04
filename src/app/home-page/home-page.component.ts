7cb74c818768f9a100d4169bf4b91a90fd56cd7e
Jesse <j.hoed@xs4all.nl>
2024-03-04 01:12:20 +0100

    Added rest of homepage, added footer

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
+  imports: [NgbCarouselModule],
+  standalone: true,
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
